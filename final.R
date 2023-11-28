73158882499ccc7708898f5e08158910fa0af218
kenx128 <kenx128@uw.edu>
2023-11-27 22:37:23 -0800

    Cleaned and merged the datasets

--- final.R
+++ final.R
@@ -1,5 +1,6 @@
 library(dplyr)
 library(stringr)
+library(tidyr)
 
 
 # Load in dataset
@@ -10,3 +11,27 @@ housing_df <- read.csv("Housing_Data.csv")
 # Add new column state_code that stores two letters abbreviation
 housing_df <- mutate(housing_df,
                      state_code = state.abb[match(housing_df$State.Name, state.name)])
+
+homeless_df <- filter(homeless_df,
+                      homeless_df$Year >= '1/1/2007',
+                      homeless_df$Year <= '1/1/2011')
+
+housing_df <- filter(housing_df,
+                     housing_df$Start.Year >= '1/1/2007',
+                     housing_df$End.Year <= '1/1/2011')
+
+homeless_df$Count <- as.numeric(homeless_df$Count)
+
+homeless_df <- homeless_df %>%
+                 group_by(State, Measures) %>%
+                   summarise(total_count = sum(Count))
+
+homeless_df <- pivot_wider(homeless_df, names_from = Measures, values_from = total_count)
+
+df <- merge(x = housing_df,
+            y = homeless_df,
+            by.x = 'state_code',
+            by.y = 'State')
+
+df <- df %>%
+  select(-Start.Year, -End.Year, -geoid)
