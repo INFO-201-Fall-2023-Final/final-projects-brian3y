5e3b7449362092c1b9d9606acaea6e2147e62c07
kenx128 <kenx128@uw.edu>
2023-11-27 20:37:23 -0800

    added state_code column

--- final.R
+++ final.R
@@ -1,7 +1,12 @@
 library(dplyr)
 library(stringr)
 
+
 # Load in dataset
 homeless_df <- read.csv("Homeless_Data.csv")
 housing_df <- read.csv("Housing_Data.csv")
 
+
+# Add new column state_code that stores two letters abbreviation
+housing_df <- mutate(housing_df,
+                     state_code = state.abb[match(housing_df$State.Name, state.name)])
