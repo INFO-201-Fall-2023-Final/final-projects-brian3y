9af867d935749158eee5985e64292ff639921872
kenx128 <kenx128@uw.edu>
2023-12-10 04:36:56 -0800

    done with graph for shiny1, need to add descriptions etc.

--- shiny1.R
+++ shiny1.R
@@ -16,27 +16,58 @@ final_df <- mutate_dataframe(homeless_df = homeless_df, housing_df = housing_df)
 
 # Define UI
 ui <- fluidPage (
-  sidebarLayout(
-    position = 'left',
-    
-    sidebarPanel(
-      selectInput(inputId = 'state_name', 
-                  label = 'Choose a State',
-                  choices = final_df$State.Name),
-      br()
+  fluidRow(
+    column(width = 12,
+           align="center",
+           h3('Comparing Housing vs. Homeless data'),
+           selectInput(inputId = 'state_name', 
+                       label = 'Choose a State',
+                       choices = final_df$State.Name),
     ),
     
-    mainPanel(
-      h3('Comparing States'),
-      # plotlyOutput(outputId = 'scatter')
-    )
-  ),
+    br(),
+    column(width = 12,
+           plotlyOutput(outputId = 'bar', width = "100%", height = "500px"))
+  )
 )
 
 
 # Define server logic
 server <- function(input, output) {
+  output$bar <- renderPlotly({
+    
+    if(!is.null(input$state_name) && input$state_name != '') {
+      state_df <- filter(final_df,
+                         final_df$State.Name == input$state_name,
+                         final_df$Tenure == 'Owner occupied')
+      
+      filtered_state <- state_df %>% 
+        select(County.Name, Total.Households)
+      
+      homeless_count <- state_df$`Total Homeless`[1]
+      homeless_row <- data.frame(County.Name = "Total Homeless", Total.Households = homeless_count)
+      filtered_state <- rbind(filtered_state, homeless_row) 
+      
+      filtered_state <- arrange(filtered_state, desc(filtered_state$Total.Households))
+      filtered_state$County.Name <- factor(filtered_state$County.Name, levels = filtered_state$County.Name)
   
+      p <- ggplot(filtered_state,
+                  aes(x = County.Name,
+                      y = Total.Households,
+                      fill = County.Name)) +
+           geom_bar(stat='identity') +
+           labs(x = "County + Homeless",
+                y = "Population",
+                fill = 'County + Homeless') +
+           theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+           scale_fill_manual(values = ifelse(filtered_state$County.Name == 'Total Homeless', 'red', 'steelblue'))
+      
+      p <- ggplotly(p)
+      return(p)
+    } else {
+      return(NULL)
+    }
+  })
 }
 
 
