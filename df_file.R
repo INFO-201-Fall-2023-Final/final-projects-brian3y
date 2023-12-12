8a1546e409d7de7d2f6bdf275a8ca6fbd06a359c
13364 <jyuan7@uw.edu>
2023-12-11 17:13:00 -0800

    finals

--- df_file.R
+++
@@ -1,3 +0,0 @@
-
-df <- read.csv("final.csv")
-distinct_state <- slice(group_by(df, State.Name), 1)
--- multi_shiny.R
+++
@@ -1,9 +0,0 @@
-# app.R
-library(shiny)
-
-# Source the UI, server, and global files
-source("shiny1.R")
-source("shiny2.R")
-
-# Run the Shiny app
-shinyApp(ui = ui, server = server)
